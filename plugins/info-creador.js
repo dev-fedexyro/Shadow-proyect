ed34840f499d7f6ed6a09bb3ae1e07894257afb2
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-27 09:45:54 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,11 +1,14 @@
-let handler = async (m, { conn}) => {
+let handler = async (m, { conn }) => {
     const vcard = `BEGIN:VCARD
 VERSION:3.0
 FN:𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚
 ORG:𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚
+TITLE:Epictetus, Enchiridion — Chapter 1 (verse 1)
 TEL;type=CELL;waid=5491124918653:+5491124918653
-URL;type=WORK:https://www.instagram.com/dev-fedexyz13
+ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;Izumi;Osaka;594-0071;Japan
+X-WA-BIZ-NAME:Ｓｈａｄｏｗ - Ｂｏｔ
 X-WA-BIZ-DESCRIPTION:ꜱʜᴀᴅᴏᴡ, ʙᴏᴛ ᴄʀᴇᴀᴅᴏ ꜱᴏʟᴏ ᴘᴏʀ ᴅɪᴠᴇʀꜱɪóɴ ᴜᴡᴜ ♡
+X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
 END:VCARD`;
 
     const q = {
@@ -13,22 +16,22 @@ END:VCARD`;
             fromMe: false,
             participant: "0@s.whatsapp.net",
             remoteJid: "status@broadcast",
-},
+        },
         message: {
             contactMessage: {
                 displayName: "𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚",
                 vcard,
-},
-},
-};
+            },
+        },
+    };
 
     await conn.sendMessage(
         m.chat,
         {
             contacts: {
                 displayName: "𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚",
-                contacts: [{ vcard}],
-},
+                contacts: [{ vcard }],
+            },
             contextInfo: {
                 externalAdReply: {
                     title: "𝘊𝘳𝘦𝘢𝘥𝘰𝘳 𝘥𝘦𝘭 𝘉𝘰𝘵 𝘹 𝘧𝘦𝘥𝘦 𝘜𝘤𝘩𝘪𝘩𝘢",
@@ -36,11 +39,11 @@ END:VCARD`;
                     thumbnailUrl: "https://files.catbox.moe/2xlrwj.jpg",
                     mediaType: 1,
                     renderLargerThumbnail: true,
-},
-},
-},
-        { quoted: q}
-);
+                },
+            },
+        },
+        { quoted: q }
+    );
 };
 
 handler.help = ["owner"];
